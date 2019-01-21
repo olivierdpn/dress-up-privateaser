6bc8d04cfc6ae218b615456dee192e360cdf7685
Olivier Dupain <olivier.dupain@devinci.fr>
2019-01-21 13:10:13 +0100

    update

--- public/index.js
+++ public/index.js
@@ -4,24 +4,51 @@
 (() => {
   const render = (actors) => {
     const fragment = document.createDocumentFragment();
+
     const div = document.createElement('div');
-    const template = actors.map(actor => {
+    var template = `
+    <div class="card-group">
+    `;
+    template += actors.map(actor => {
+      if(actor.type == "credit")
       return `
-        <div class="actor">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
+        <div class="card border-success text-center">
+        <div class="card-body text-success">
+        <h5 class="card-title">${actor.who}</h5>
+        <p class="card-text">${actor.amount} €</p>
+        </div>
+        </div>
+      `;
+      else 
+        return`
+        <div class="card text-center border-danger">
+        <div class="card-body text-danger">
+        <h5 class="card-title">${actor.who}</h5>
+        <p class="card-text">${actor.amount} €</p>
+        </div>
         </div>
       `;
     }).join('');
-
+    template += `
+    </div>
+    `;
     div.innerHTML = template;
+
     fragment.appendChild(div);
+
+ 
+
     document.querySelector('#actors').innerHTML = '';
     document.querySelector('#actors').appendChild(fragment);
   };
 
+
+
+
   const button = document.querySelector('#compute');
+
+
+
 
   button.addEventListener('click', function onClick () {
     const bar = PRIVATEASER.getBar();
@@ -29,6 +56,8 @@
     const persons = document.querySelector('.js-persons').value;
     const option = document.querySelector('.js-option').checked;
     const actors = PRIVATEASER.payActors(bar, time, persons, option);
+
+   document.getElementById("compute").value = "↓ Your quote below ↓ (click to ask a new quote !)";
 
     render(actors);
 
